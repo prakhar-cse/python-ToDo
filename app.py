c14f8ef4ec1109f459c47404abd6d6ca87b523d0
prakhar-cse <prakhar.cse.gbu@gmail.com>
2026-02-20 18:10:02 +0530

    added create and read task op

--- app.py
+++ app.py
@@ -16,15 +16,54 @@ def connect():
     return sqlite3.connect("todo.db")
 
 def add_task(task):
+    if task is not "":
+        conn = connect()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO todos (task) VALUES (?)",
+            (task,)
+        )
+
+        conn.commit()
+        conn.close()
+
+        print("Task is added")
+    else:
+        print("You can not add Null task")
+
+def get_all_tasks():
     conn = connect()
     cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO todos (task) VALUES (?)",
-        (task,)
-    )
-
-    conn.commit()
+    cursor.execute("SELECT * FROM todos")
+    tasks = cursor.fetchall()
     conn.close()
 
-    print("Task is added")
+    print("***************ALL TASKS**************")
+    for task in tasks:
+        status = "Done" if task[2] else "---"
+        print(f"{task[0]},      {task[1]},      {status}")
+
+
+
+def main():
+
+    print("**********************************************")
+    print("******************WELCOME*********************")
+    print("**********************************************")
+
+    while True:
+        choice = input("Enter your action: ")
+        if(choice == "1"):
+            get_all_tasks()
+        elif choice == "2":
+            task = input("Enter task- ")
+            add_task(task)
+        else:
+            print("Closing todo app")
+            break
+
+
+if __name__ == "__main__":
+    main()
